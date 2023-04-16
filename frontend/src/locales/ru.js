2bf07e8d80810bb012912c04c5ec3dcd07c7586a
Nadezhda Kamenskaya <nady.kamenskaya@gmail.com>
2023-04-16 18:45:06 +0300

    Add ru lang

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,53 @@
+const ru = {
+  translation: {
+    hexletChat: 'Hexlet Chat',
+    buttons: {
+      addChannel: 'Добавить канал',
+      cancel: 'Отменить',
+      channelManagement: 'Управление каналом',
+      logIn: 'Войти',
+      logOut: 'Выйти',
+      register: 'Зарегистрироваться',
+      remove: 'Удалить',
+      removeChannel: 'Удалить канал',
+      rename: 'Переименовать',
+      renameChannel: 'Переименовать канал',
+      submit: 'Отправить',
+    },
+    errors: {
+      alreadyExists: 'Такой пользователь уже существует',
+      checkPassword: 'Пароли должны совпадать',
+      confirmPassword: 'Подтвердите пароль',
+      incorrect: 'Неверные имя пользователя или пароль',
+      minLetter: 'Не менее 6 символов',
+      notOneOf: 'Должно быть уникальным',
+      rangeLetter: 'От 3 до 20 символов',
+      required: 'Обязательное поле',
+    },
+    fields: {
+      inputMessage: 'Введите сообщение...',
+      nickname: 'Ваш ник',
+      password: 'Пароль',
+      username: 'Имя пользователя',
+    },
+    ui: {
+      channels: 'Каналы',
+      goTo: 'Но вы можете перейти',
+      hexletChat: 'Hexlet Chat',
+      mainPage: 'на главную страницу',
+      nameChannel: 'Имя канала',
+      newMessage: 'Новое сообщение',
+      noAccount: 'Нет аккаунта? ',
+      notFoundPage: 'Страница не найдена',
+      proof: 'Уверены?',
+      registration: 'Регистрация',
+      counter: {
+        count_one: '{{count}} сообщение',
+        count_few: '{{count}} сообщения',
+        count_many: '{{count}} сообщений',
+      },
+    },
+  },
+};
+
+export default ru;
